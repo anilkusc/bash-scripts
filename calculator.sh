e4fb641bcdeb9a50e34dd131de6e6c510bd17ffd
Anıl Kuşçu <63120231+anilkusc@users.noreply.github.com>
2020-12-15 22:29:24 +0300

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+add(){
+val=$(($1 + $2))
+return $val
+}
+
+subs(){
+val=$(($1-$2))
+return $val
+}
+
+mult(){
+val=$(($1*$2))
+return $val
+}
+
+div(){
+val=$(($1/$2))
+return $val
+}
+
+echo "give me first number:"
+read a
+echo "give me second number:"
+read b
+echo "give me operator:"
+read o
+
+case "$o" in
+   "+")
+        add $a $b
+   ;;
+   "-")
+        subs $a $b
+   ;;
+   "*")
+        mult $a $b
+   ;;
+   "/")
+        div $a $b
+   ;;
+   *)
+        echo "wrong operator"
+   ;;
+esac
+
+echo $?
