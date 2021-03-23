09e7b4132ee5d5fc916e3e449521bb1594de943d
Anıl Kuşçu <63120231+anilkusc@users.noreply.github.com>
2021-03-23 17:24:16 +0300

    Update recover-mongo.sh

--- recover-mongo.sh
+++ recover-mongo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # This script is salvage your wired tiger mongo collection and after that you be able to repair and use your corrupted mongo database again
+apt-get update && apt-get install libsnappy-dev build-essential wget -y
 wget http://source.wiredtiger.com/releases/wiredtiger-2.7.0.tar.bz2
 tar xvf wiredtiger-2.7.0.tar.bz2
 cd wiredtiger-2.7.0
- apt-get install libsnappy-dev build-essential -y
+
 ./configure --enable-snappy
 make
 
